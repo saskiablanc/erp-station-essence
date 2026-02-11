669da9a697e2f1870080bfd0da92781a0fa536a5
blanc saskia <saskia.blanc@etu.univ-cotedazur.fr>
2026-02-11 09:16:45 +0100

    Reset: Migration vers stack PHP/JS/CSS

--- backend/models/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Package des modèles SQLAlchemy.
-Importer tous les modèles ici pour qu'Alembic les détecte.
-"""
-
-from backend.models.client import Client
-
-# Liste des modèles exportés
-__all__ = ['Client']
--- database/init_empty.sql
+++
@@ -1,4 +0,0 @@
-SELECT DATABASE() AS 'Base de données active';
-
--- Message de confirmation
-SELECT 'Base de données initialisée avec succès' AS 'Status';
